f41354e394028123e2c0f351d0d9aa9c934f8674
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2016-04-20 16:23:01 +0200

    improve javascript snippet to support async load
    the snippet no longer depends on "defer" working exactly as specified, ie.
    running when the page has been parsed but before DOMContentLoaded. this
    allows loading it with "async" so that it doesn't delay the page at all.

--- src/main/resources/de/uniKonstanz/shib/disco/header.js
+++ src/main/resources/de/uniKonstanz/shib/disco/header.js
@@ -1,13 +1,13 @@
 function shibbolethDiscovery(base, html) {
-	var insert = function() {
+	function insertDiscovery() {
 		// insert the CSS, as a DOM element
 		var link = document.createElement("link");
 		link.rel = "stylesheet";
 		link.type = "text/css";
 		link.href = base + "/disco.css";
 		document.getElementsByTagName("head")[0].appendChild(link);
+
 		// create tag containing discovery HTML snippet
-		var target = document.getElementById("shibboleth-discovery");
 		var container = document.createElement("div");
 		container.id = "shibboleth-discovery";
 		if ("undefined" != typeof jQuery)
@@ -22,21 +22,29 @@ function shibbolethDiscovery(base, html) {
 		// loading full discovery with turbolinks breaks the search
 		if ("undefined" != typeof container.setAttribute)
 			container.setAttribute("data-no-turbolink", "true");
+
 		// replace "shibboleth-discovery" with HTML snippet. this allows
 		// for graceful failure when there is no javascript.
+		var target = document.getElementById("shibboleth-discovery");
 		target.parentNode.replaceChild(container, target);
 	}
-	
+
 	if ("undefined" != typeof jQuery)
-		// jquery, present on most pages these days
-		jQuery(insert);
+		// use jquery, present on most pages these days
+		jQuery(insertDiscovery);
+	else if (document.getElementById("shibboleth-discovery"))
+		// page already loaded far enough; no need to wait.
+		// important when loading asynchronously, because
+		// DOMContentLoaded might already have been fired in this case.
+		insertDiscovery();
 	else if ("undefined" != typeof document.addEventListener)
-		// all modern browsers
-		document.addEventListener("DOMContentLoaded", insert);
+		// all modern browsers: wait for page loaded
+		document.addEventListener("DOMContentLoaded", insertDiscovery, false);
 	else if ("undefined" != typeof document.attachEvent)
 		// IE8 legacy
 		document.attachEvent("onreadystatechange", function(){
 			if (document.readyState === "complete")
-				insert();
+				insertDiscovery();
 		});
+	// else give up. the fallback button must do.
 }
